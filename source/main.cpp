b7812e86e28b7181516bbcd8211b076eaffa7786
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-21 21:39:33 +0300

    Remove stub implementation

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include <string>
-
-#include "lib.hpp"
-
 auto main() -> int
 {
-  auto const lib = library {};
-  auto const message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
   return 0;
 }
--- test/source/libcurves-demo_test.cpp
+++ test/source/libcurves-demo_test.cpp
@@ -1,9 +1,1 @@
 #include <catch2/catch_test_macros.hpp>
-
-#include "lib.hpp"
-
-TEST_CASE("Name is libcurves-demo", "[library]")
-{
-  auto const lib = library {};
-  REQUIRE(lib.name == "libcurves-demo");
-}
